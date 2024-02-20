7e7b9daeb2b7596bad37e27ca3db891582d4f359
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-02-20 05:45:12 -0500

    feat: use {} to render JS

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,8 +1,11 @@
+const getSaludo = (name) => `Hola, soy ${name}`;
+
 export const FirstApp = () => {
     return (
         // <> es una abreviación de un Fragmento, en REACT siempre se debe retornar un nodo padre
+        // Se usa {} para renderizar JS dentro del XML representando el DOM
         <>
-            <h1>Andrés Aristizábal</h1>
+            <h1>{getSaludo('Andrés')}</h1>
             <p>Soy un desarrollador full stack</p>
         </>
     );
