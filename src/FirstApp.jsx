9bdfb985273c360639037fa2ba174fa4f0b3acac
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-02-20 05:41:07 -0500

    feat: add a fragment

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,5 +1,9 @@
 export const FirstApp = () => {
     return (
-        <h1>Andrés Aristizábal</h1>
+        // <> es una abreviación de un Fragmento, en REACT siempre se debe retornar un nodo padre
+        <>
+            <h1>Andrés Aristizábal</h1>
+            <p>Soy un desarrollador full stack</p>
+        </>
     );
 };
