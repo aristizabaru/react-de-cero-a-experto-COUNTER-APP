813ac4e969a624f678f040db89093ee678c0fb6e
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-02-20 06:23:24 -0500

    feat: send props to FirstApp component

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,12 +1,11 @@
-const getSaludo = (name) => `Hola, soy ${name}`;
+export const FirstApp = ({ title, subTitle }) => {
 
-export const FirstApp = () => {
     return (
         // <> es una abreviación de un Fragmento, en REACT siempre se debe retornar un nodo padre
         // Se usa {} para renderizar JS dentro del XML representando el DOM
         <>
-            <h1>{getSaludo('Andrés')}</h1>
-            <p>Soy un desarrollador full stack</p>
+            <h1>{title}</h1>
+            <p>{subTitle}</p>
         </>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { FirstApp } from './FirstApp';
+import './styles.css';
 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 root.render(
   <React.StrictMode>
-    <FirstApp />
+    <FirstApp title='Bienvenido a REACT' subTitle='De cero a experto' />
   </React.StrictMode>
 );
